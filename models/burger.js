4af32414012bc61b9a8e37eef2aa40fe2fd7c84a
Noah Soukup <noahsoukupraesch@gmail.com>
2020-07-17 16:58:12 -0500

    model created

---
+++ models/burger.js
@@ -0,0 +1,25 @@
+var orm = require("../config/orm.js")
+
+var burger = {
+    all: (cb) => {
+        orm.all("burgers", (res) => {
+            cb(res);
+        });
+    },
+    create: (cols, vals, cb) => {
+        orm.create("burgers", cols, vals, (res) => {
+            cb(res);
+        });
+    },
+    update: (objColVals, condition, cb) => {
+        orm.update("burgers", objColVals, condition, (res) => {
+            cb(res);
+        });
+    },
+    delete: (condition, cb) => {
+        orm.delete("burgers", condition, (res) => {
+            cb(res);
+        })
+    }
+}
+module.exports = burger;
