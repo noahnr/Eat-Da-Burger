979cd48c35e74ad970961b15098b30b68eb5a77a
Noah Soukup <noahsoukupraesch@gmail.com>
2020-07-17 16:41:22 -0500

    orm done

--- config/orm.js
+++ config/orm.js
@@ -58,4 +58,24 @@ var orm = {
       cb(result);
     });
   },
-}
+  update: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  }
+};
+
+// Export the orm object for the model (cat.js).
+module.exports = orm;
